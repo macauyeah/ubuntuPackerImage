cde00fbb9b2ab6b1a2d96ea747a59b548f41ac98
macauyeah <macauyeahwebmail@gmail.com>
2024-08-19 13:00:33 +0800

    fetch the new apt package could fix docker image pull problem; ssh port forward should relay on host IPv4, connot listen on 0.0.0.0

--- initCephCluster.sh
+++ initCephCluster.sh
@@ -33,48 +33,26 @@ done
 rm $tmpNetplanFile
 
 multipass exec -n ${nodeName[0]} -- sudo apt install -y cephadm
-# https://quay.ceph.io/repository/ceph-ci/ceph?tab=tags&tag=master
-# ? docker image pull quay.ceph.io/ceph-ci/ceph:master
-multipass exec -n ${nodeName[0]} -- sudo docker image pull quay.io/ceph/ceph:v17
-multipass exec -n ${nodeName[0]} -- sudo docker image pull quay.io/ceph/ceph-grafana:9.4.7
-multipass exec -n ${nodeName[0]} -- sudo docker image pull quay.io/prometheus/prometheus:v17
-multipass exec -n ${nodeName[0]} -- sudo docker image pull quay.io/prometheus/alertmanager:v0.25.0
-multipass exec -n ${nodeName[0]} -- sudo docker image pull quay.io/prometheus/node-exporter:v1.5.0
-
-multipass exec -n ${nodeName[0]} -- sudo cephadm bootstrap --mon-ip ${ip[0]}
-
-# This is a development version of cephadm.
-# For information regarding the latest stable release:
-#     https://docs.ceph.com/docs/pacific/cephadm/install
-# Verifying podman|docker is present...
-# Verifying lvm2 is present...
-# Verifying time synchronization is in place...
-# Unit systemd-timesyncd.service is enabled and running
-# Repeating the final host check...
-# docker (/usr/bin/docker) is present
-# systemctl is present
-# lvcreate is present
-# Unit systemd-timesyncd.service is enabled and running
-# Host looks OK
-# Cluster fsid: 7286a7ea-5b82-11ef-8f1d-71f225c9586b
-# Verifying IP 10.13.31.21 port 3300 ...
-# Verifying IP 10.13.31.21 port 6789 ...
-# Mon IP `10.13.31.21` is in CIDR network `10.13.31.0/24`
-# - internal network (--cluster-network) has not been provided, OSD replication will default to the public_network
-# Pulling container image quay.ceph.io/ceph-ci/ceph:master...
-# Non-zero exit code 1 from /usr/bin/docker pull quay.ceph.io/ceph-ci/ceph:master
-# /usr/bin/docker: stderr Error response from daemon: manifest for quay.ceph.io/ceph-ci/ceph:master not found: manifest unknown: manifest unknown
-
+multipass exec -n ${nodeName[0]} -- sudo cephadm bootstrap --mon-ip ${ip[0]} > fileContianDefaultDashboardAdminPassword.txt
 
 cephPub=$(multipass exec -n ${nodeName[0]} -- cat /etc/ceph/ceph.pub)
 echo "$cephPub" | multipass exec -n ${nodeName[1]} -- sudo tee -a /root/.ssh/authorized_keys
 echo "$cephPub" | multipass exec -n ${nodeName[2]} -- sudo tee -a /root/.ssh/authorized_keys
 
-multipass exec -n ${nodeName[1]} -- sudo apt-get install docker.io
-multipass exec -n ${nodeName[2]} -- sudo apt-get install docker.io
+multipass exec -n ${nodeName[1]} -- sudo apt-get install -y docker.io
+multipass exec -n ${nodeName[2]} -- sudo apt-get install -y docker.io
+multipass exec -n ${nodeName[0]} -- sudo cephadm shell -- ceph orch host add ${nodeName[1]} ${ip[1]} _admin
+multipass exec -n ${nodeName[0]} -- sudo cephadm shell -- ceph orch host add ${nodeName[2]} ${ip[2]} _admin
+multipass exec -n ${nodeName[0]} -- sudo cephadm shell -- ceph orch host ls --detail
 
-# for port forwarding
+multipass transfer fileContianDefaultDashboardAdminPassword.txt ${nodeName[0]}:./
+rm fileContianDefaultDashboardAdminPassword.txt
+
 echo force update known_hosts public key of ${ip[0]}
 sudo ssh-keygen -R ${ip[0]}
 echo port forwarding to ${ip[0]}
-sudo ssh -i /var/snap/multipass/common/data/multipassd/ssh-keys/id_rsa -L 0.0.0.0:8443:${ip[0]}:8443 ubuntu@${ip[0]}
+localMachinePublicIp=$(hostname -I | cut -d' ' -f1)
+# hardcode local machine ipv4 public ip if above command not work well
+sudo ssh -i /var/snap/multipass/common/data/multipassd/ssh-keys/id_rsa -L $localMachinePublicIp:8443:${ip[0]}:8443 ubuntu@${ip[0]}
+
+# cat fileContianDefaultDashboardAdminPassword.txt in node21 if you need dashboard login;
